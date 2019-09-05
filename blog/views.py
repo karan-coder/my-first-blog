b0b1b4f12bd2fa4e1b53eb450a732d7a3aa7722b
Karan <karan.artomate@studio>
2019-09-05 17:00:28 +0530

    changed the html part of the site

--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,9 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+
+
+def post_list(request):
 # Create your views here.
+
+	return render(request, 'blog/post_list.html', {})
